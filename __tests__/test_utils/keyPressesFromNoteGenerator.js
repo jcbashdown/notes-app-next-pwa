485fbb88c2e3a6240a460db09296a66d70e0c9f1
Jacob Ashdown <jcbashdown@gmail.com>
2024-06-10 12:04:29 +0300

    Add test helpers for e2e

---
+++ __tests__/test_utils/keyPressesFromNoteGenerator.js
@@ -0,0 +1,67 @@
+export const FOCUS_CHANGERS = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter']
+
+export default class KeyPressesFromNoteGenerator {
+    constructor() {
+        this.keyPresses = []
+        this.note = null
+    }
+
+    setNote = (note) => {
+        this.note = note
+    }
+
+    getKeyPresses = () => {
+        return this.generateKeyPresses()
+    }
+
+    generateKeyPresses() {
+        const note = this.note
+        const lines = note.split('\n')
+        let keyPresses = lines
+            .reduce((memo, item) => {
+                if (item === '') return memo
+                const withTabs = item.replace(/\s{4}/g, '')
+                const withCommandsSplitOut = withTabs.split('|')
+                const lineText = withCommandsSplitOut[0]
+                const withLeftAndRight = withCommandsSplitOut.reduce((memo, item) => {
+                    if (item === 'downWithArrowRight' || item === 'upWithArrowLeft') {
+                        memo = memo.concat(this.expandLeftAndRight(item, lineText))
+                    } else {
+                        memo.push(item)
+                    }
+                    return memo
+                }, [])
+                memo = memo.concat(withLeftAndRight)
+                memo.push('Enter')
+                return memo
+            }, [])
+            .filter((keyPress) => keyPress !== '')
+        //remove the last keyPress if it's enter
+        const last = keyPresses.pop()
+        if (last === 'Enter') {
+            return keyPresses
+        } else {
+            keyPresses.push(last)
+            return keyPresses
+        }
+    }
+
+    /*
+     * upWithArrowLeft and downWithArrowRight must be the first navigation commands on the line otherwise the number or presses will be wrong
+     * This still allows us to test arrow right and left
+     * If we need something else then you have to manually type the number of ArrowLefts and Rights
+     */
+    expandLeftAndRight(command, lineText) {
+        let commandUnit = ''
+        if (command === 'downWithArrowRight') {
+            commandUnit = 'ArrowRight'
+        } else {
+            commandUnit = 'ArrowLeft'
+        }
+        if (lineText[0] === '+' || lineText[0] === '-') {
+            return Array(lineText.length).fill(commandUnit)
+        } else {
+            return Array(lineText.length + 1).fill(commandUnit)
+        }
+    }
+}
---
+++ __tests__/test_utils/keyPressesFromNoteGenerator.test.js
@@ -0,0 +1,127 @@
+import KeyPressesFromNoteGenerator from './keyPressesFromNoteGenerator'
+
+const simpleNoteInput = `
+A new Note
+    + This note supports "a new Note"
+    - This note opposes "a new Note"
+        This is related to the opposing note|Tab|
+    This note is related to a new Note.`
+
+const complexNoteInput = `
+A new Note
+    - This note opposes "a new Note"
+        This is related to the opposing note|Tab|upWithArrowLeft|ArrowUp|
+    + This note supports "a new Note"|downWithArrowRight|ArrowDown|ArrowDown|
+    |Backspace|This note is related to a new Note.
+`
+
+describe('keyPressesFromNoteGenerator', () => {
+    it('should return an array of key presses for a simple note', () => {
+        const keyPressesFromNoteGenerator = new KeyPressesFromNoteGenerator()
+        keyPressesFromNoteGenerator.setNote(simpleNoteInput)
+        const keyPresses = keyPressesFromNoteGenerator.getKeyPresses()
+        expect(keyPresses).toEqual([
+            'A new Note',
+            'Enter',
+            '+ This note supports "a new Note"',
+            'Enter',
+            '- This note opposes "a new Note"',
+            'Enter',
+            'This is related to the opposing note',
+            'Tab',
+            'Enter',
+            'This note is related to a new Note.',
+        ])
+    })
+    it('should return an array of key presses for a complex note', () => {
+        const keyPressesFromNoteGenerator = new KeyPressesFromNoteGenerator()
+        keyPressesFromNoteGenerator.setNote(complexNoteInput)
+        const keyPresses = keyPressesFromNoteGenerator.getKeyPresses()
+        expect(keyPresses).toEqual([
+            'A new Note',
+            'Enter',
+            '- This note opposes "a new Note"',
+            'Enter',
+            'This is related to the opposing note',
+            'Tab',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowLeft',
+            'ArrowUp',
+            'Enter',
+            '+ This note supports "a new Note"',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowRight',
+            'ArrowDown',
+            'ArrowDown',
+            'Enter',
+            'Backspace',
+            'This note is related to a new Note.',
+        ])
+    })
+})
